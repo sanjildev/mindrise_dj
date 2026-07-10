5596351b404d05a5a178cf56a850007b41126b51
sanjildev <shakyasanjil00@gmail.com>
2026-07-10 09:06:59 +0545

    template add

--- app1/views.py
+++ app1/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("This is index page.")
+    context={
+        "body":"Welcome .This is Index Page."
+    }
+    return render(request,'index.html',context)
 
 def home(request):
-    return HttpResponse('Welcome,This is home page')
+    return render(request,'home.html')
 
 def about_us(request):
     return HttpResponse("Welcome,This is About Us page!!")
