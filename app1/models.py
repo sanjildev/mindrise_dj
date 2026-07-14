e38913501a1c6aebb2788398576fa773f5628912
sanjildev <shakyasanjil00@gmail.com>
2026-07-14 09:01:10 +0545

    make todolist model

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todolist(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    status=models.BooleanField(default=False)
+    date=models.DateField()
+
